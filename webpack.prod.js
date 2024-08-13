fcb798e1fe5079f290174b823d029866fda49ea9
Rafael Arias <137901995+rariasnav@users.noreply.github.com>
2024-08-13 23:22:22 +0000

    modified render.yaml

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,11 @@ const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
 module.exports = merge(common, {
     mode: 'production',
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
     output: {
         publicPath: '/'
     },
